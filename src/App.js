e12aa4c0d9b7175282dcabae515b48429cf3dccf
Adeesha97 <adeeshakawshal1997@gmail.com>
2022-05-14 20:01:06 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Home from "./pages/Home";
+// import ProductList from "./pages/ProductList";
+// import SingleProduct from "./pages/SingleProduct";
+// import Register from "./pages/Register";
+// import Login from "./pages/Login";
+// import Cart from "./pages/Cart";
+
+function App() {
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
